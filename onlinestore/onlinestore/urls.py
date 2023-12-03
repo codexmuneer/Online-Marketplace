7631c6b634dfd16fd2a56a7ba3cc510e73594f95
codexmuneer <muneerhanif7@gmail.com>
2023-12-03 12:21:40 -0800

    all files added

---
+++ onlinestore/onlinestore/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('inbox/', include('conversation.urls')),
+    path('' , include('core.urls')),
+    path('dashboard/', include('dashboard.urls')),
+    path('items/', include('item.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
